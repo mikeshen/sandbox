fcaba668bf116d6c36e18fade205707b553016f3
Mike Shen <mikeshen@umich.edu>
2024-08-19 22:14:22 +0000

    Add palindrome problem.

---
+++ cpp_problems/palindrome.cpp
@@ -0,0 +1,50 @@
+#include "template.h"
+#include "test_runner.h"
+
+string processString(const std::string& str) {
+    string result = str;
+
+    // Remove non-whitespace and punctuation characters
+    result.erase(remove_if(result.begin(), result.end(),
+                            [](char c) { return !isalnum(c); }),
+                    result.end());
+
+    // Convert to lowercase
+    std::transform(result.begin(), result.end(), result.begin(),
+                    [](unsigned char c) { return std::tolower(c); });
+
+    return result;
+}
+
+bool isPalindrome(string s) {
+    s = processString(s);
+    if (s.size() == 1)
+        return true;
+    int i = 0, j = s.size() - 1;
+    while (i <= j && s[i] == s[j]) {
+        i++;
+        j--;
+    }
+    return i > j;
+}
+
+void testPalindrome() {
+    customAssert(isPalindrome("racecar"));
+    customAssert(isPalindrome("race car"));
+}
+
+void runTests() {
+    vector<string> testResults;
+    testResults.push_back(runTest("testPalindrome", testPalindrome));
+
+    // Print test results
+    for (const auto& result : testResults) {
+        cout << result << endl;
+    }
+}
+
+int main() {
+    runTests();
+    return 0;
+}
+
